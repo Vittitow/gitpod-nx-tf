0a8a74337f9f2df6047989b7eeaa23f4fe920e6c
Vittitow <corey.vittitow@gmail.com>
2022-01-24 16:04:32 +0000

    add remaining environments for demo

--- apps/intapis/nciyes/src/outputs.tf
+++ apps/intapis/nciyes/src/outputs.tf
@@ -1,9 +1,9 @@
 output "intapis_core_resource_group_name" {
-  value       = data.terraform_remote_state.intapis_core.resource_group_name
+  value       = data.terraform_remote_state.intapis_core.outputs.resource_group_name
   description = "intapis-core remote state resource group name"
 }
 
 output "intapis_shared_resource_group_name" {
-  value       = data.terraform_remote_state.intapis_shared.resource_group_name
+  value       = data.terraform_remote_state.intapis_shared.outputs.resource_group_name
   description = "intapis-shared remote state resource group name"
 }
--- apps/intapis/shared/src/outputs.tf
+++ apps/intapis/shared/src/outputs.tf
@@ -1,4 +1,4 @@
 output "intapis_core_resource_group_name" {
-  value       = data.terraform_remote_state.intapis_core.resource_group_name
+  value       = data.terraform_remote_state.intapis_core.outputs.resource_group_name
   description = "intapis-core remote state resource group name"
 }
